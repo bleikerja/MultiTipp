5be4d2cf444609b0020562ee19894e0f5a0d435d
bleikerja <bleikerja@gmail.com>
2025-07-19 13:38:28 +0200

    css version change

--- games.php
+++ games.php
@@ -11,7 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <title>Multi Tipp</title>
-    <link rel="stylesheet" href="styles/games.css?v=11">
+    <link rel="stylesheet" href="styles/games.css?v=12">
     <link rel="stylesheet" href="styles/navBar.css?v=2">
 </head>
 <body>
